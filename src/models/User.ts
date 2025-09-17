56a2b0adef6e465942f81fec6daa3d8e224f92f1
Mouhamed Doumbia <doumbiasoft@gmail.com>
2025-09-17 14:41:14 -0400

    implemented user schema and validation

---
+++ src/models/User.ts
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+import { ValidationPatterns } from "../middlewares/validation.middleware";
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+export type Users = User[];
+
+const emailValidators = [
+  {
+    // Validator 1: Check if the email is valid.
+    validator: function (email: string) {
+      return ValidationPatterns.EMAIL.test(email);
+    },
+    message: (props: any) =>
+      `${props.path} (${props.value}) is not a valid email address.`,
+  },
+  {
+    validator: async function (email: string) {
+      // Query the database to see if another user exists with the same email
+      const user = await mongoose.models.User.findOne({ email });
+      // Return true if no other user is found, indicating a unique email
+      return !user;
+    },
+    message: (props: any) =>
+      `${props.value} is already in use. Please choose a different email.`,
+  },
+];
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+    minLength: [3, "The name length should not be less than 3"],
+    maxLength: [50, "The name length should not be over 50"],
+  },
+  username: {
+    type: String,
+    require: true,
+    unique: true,
+    lowerCase: true,
+    validate: {
+      validator: async function (username: string) {
+        // Query the database to see if another user exists with the same username
+        const user = await mongoose.models.User.findOne({ username });
+        // Return true if no other user is found, indicating a unique username
+        return !user;
+      },
+      message: (props: any) =>
+        `${props.value} is already in use. Please choose a different username.`,
+    },
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+    lowerCase: true,
+    validate: emailValidators,
+  },
+  createdAt: { type: Date, default: Date.now() },
+  updatedAt: { type: Date, default: () => Date.now() },
+});
+
+userSchema.statics.getCount = async function () {
+  return await mongoose.models.User.countDocuments();
+};
+
+export default mongoose.model("User", userSchema);
