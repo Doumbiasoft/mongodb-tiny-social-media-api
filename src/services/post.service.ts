ebd25b268214ca7eec01b9628bc4112d95c0322e
Mouhamed Doumbia <doumbiasoft@gmail.com>
2025-09-17 20:32:20 -0400

    implemented post service

---
+++ src/services/post.service.ts
@@ -0,0 +1,23 @@
+import PostModel, { IPost } from "../models/Post";
+
+export const findAllPosts = async (userId?: string) => {
+  const filter: any = {};
+  if (userId) {
+    filter.userId = userId;
+  }
+  return await PostModel.find(filter);
+};
+export const findPostById = async (postId: string) => {
+  return await PostModel.findById({ _id: postId });
+};
+export const createPost = async (post: Partial<IPost>) => {
+  return await PostModel.create({ post });
+};
+export const updatePostById = async (postId: number, post: Partial<IPost>) => {
+  return await PostModel.findByIdAndUpdate({ _id: postId }, post, {
+    new: true,
+  });
+};
+export const deletePostById = async (postId: number) => {
+  return await PostModel.findByIdAndDelete({ _id: postId });
+};
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -4,41 +4,26 @@ export const findAllUsers = async () => {
   return await UserModel.find();
 };
 export const findUser = async (data: { username?: string; email?: string }) => {
-  let result = null;
+  let filter: any = {};
   if (data.username) {
-    result = await UserModel.find({
-      username: data.username?.toString().trim().toLowerCase(),
-    });
-    return result;
+    filter.username = data.username?.toString().trim().toLowerCase();
   }
   if (data.email) {
-    result = await UserModel.find({
-      username: data.email?.toString().trim().toLowerCase(),
-    });
-
-    return result;
+    filter.email = data.email?.toString().trim().toLowerCase();
   }
-  return result;
+  return await UserModel.findOne(filter);
 };
-export const findUserById = async (userId: number) => {
+export const findUserById = async (userId: string) => {
   return await UserModel.findById({ _id: userId });
 };
 export const createUser = async (user: Partial<IUser>) => {
-  const newUser: Partial<IUser> = {
-    name: user.name,
-    username: user.username,
-    email: user.email,
-  };
-  const createdUser = await UserModel.create({ newUser });
-  return createdUser;
+  return await UserModel.create({ user });
 };
-export const updateUserById = async (userId: number, user: Partial<IUser>) => {
-  const updatedUser = await UserModel.findByIdAndUpdate({ _id: userId }, user, {
+export const updateUserById = async (userId: string, user: Partial<IUser>) => {
+  return await UserModel.findByIdAndUpdate({ _id: userId }, user, {
     new: true,
   });
-  return updatedUser;
 };
-export const deleteUserById = async (userId: number) => {
-  const deletedUser = await UserModel.findByIdAndDelete({ _id: userId });
-  return deletedUser;
+export const deleteUserById = async (userId: string) => {
+  return await UserModel.findByIdAndDelete({ _id: userId });
 };
